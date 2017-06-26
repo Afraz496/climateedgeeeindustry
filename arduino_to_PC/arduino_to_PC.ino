be35a6342be8ac99aa512498a37ac12b34abf056
DelorisO <delorisowusu@gmail.com>
2017-06-26 22:21:11 +0100

    Update arduino_to_PC.ino

--- arduino_to_PC/arduino_to_PC.ino
+++ arduino_to_PC/arduino_to_PC.ino
@@ -1,72 +1,102 @@
-// This program shown how to control arduino from PC Via Bluetooth
-// Connect ...
-// arduino>>bluetooth
-// D11   >>>  Rx
-// D10   >>>  Tx
-//Written By Mohannad Rawashdeh
-//for http://www.genotronex.com/
-
-// you will need arduino 1.0.1 or higher to run this sketch
-
 #include <SoftwareSerial.h>// import the serial library
-#include <SimpleDHT.h>
+#include <SoftWire.h>
+#include <HIH61xx.h>
+#include <AsyncDelay.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 SoftwareSerial Genotronex(10, 11); // RX, TX
 int ledpin=13; // led on D13 will show blink on / off
-long previousMillis = 0;        // will store last time LED was updated
-int pinDHT11 = 2;
-SimpleDHT11 dht11;
 
-// the follow variables is a long because the time, measured in miliseconds,
-// will quickly become a bigger number than can be stored in an int.
-long interval = 1000;           // interval at which to blink (milliseconds)
-int ledState = LOW;             // ledState used to set the LED
-long Counter=0; // counter will increase every 1 second
-int i =0;
+int inByte = 0;
+int count;
+
+HIH61xx hih;
+AsyncDelay samplingInterval;
+
+// Data wire is plugged into pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices
+// (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
 
 
 void setup() {
-  // put your setup code here, to run once:
   Genotronex.begin(9600);
-  //Genotronex.println("Bluetooth On please wait....");
-  pinMode(ledpin,OUTPUT);
+  hih.initialise(A4, A5);
+  samplingInterval.start(3000, AsyncDelay::MILLIS);
+  sensors.begin();
 }
 
+bool printed = true;
 void loop() {
+if (Genotronex.available() > 0) {
 
-  byte temperature = 0;
-  byte humidity = 0;
-  byte data[40] = {0};
+inByte = Genotronex.read();
 
-  if (dht11.read(pinDHT11, &temperature, &humidity, data)) {
-    //Serial.print("Read DHT11 failed");
-    return;
+}
+
+if (samplingInterval.isExpired() && !hih.isSampling()) {
+    hih.start();
+    printed = false;
+    samplingInterval.repeat();
+    //Serial.println("Sampling started");
+    }
+
+  hih.process();
+  
+if(inByte==49){
+
+  //Put print commands here
+  //printR() -> Rainfall depth (mm)
+  printE(sensors.getTempCByIndex(0)/4); //-> Earth temp
+
+if(count==15){
+  printR(0.72);
+  printD(1.06); 
+}
+
+count++;
+  
+  //printD() -> Raindrop Size (mm)
+  if (hih.isFinished() && !printed) {
+    float temp = hih.getAmbientTemp() / 100.0;
+
+    if (temp <100){
+    printA(temp); }//-> Air Temperature
+    printH(hih.getRelHumidity() / 100.0); //-> Humidity
   }
-  
-  // put your main code here, to run repeatedly:
-  unsigned long currentMillis = millis();
+delay(2000);
 
-  if(currentMillis - previousMillis > interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;  
-     Counter+=1;
-  
-    //Genotronex.print((int)temperature);
-    //Genotronex.print("*C ");
-    Genotronex.print("19");
-    Genotronex.print("#");
-    
-    
+}
 
-    delay(2000);
-  
-    // if the LED is off turn it on and vice-versa:
-//    if (ledState == LOW)
-//      ledState = HIGH;
-//    else
-//      ledState = LOW;
-//
-//    // set the LED with the ledState of the variable:
-//    digitalWrite(ledpin, ledState);
-  }
+}
+
+void printA(float value) {
+    Genotronex.write('A');
+    Genotronex.println(value);
+}
+
+void printR(float value) {
+    Genotronex.write('R');
+    Genotronex.println(value);
+}
+
+void printE(float value) {
+    Genotronex.write('E');
+    Genotronex.println(value);
+}
+
+void printH(float value) {
+    Genotronex.write('H');
+    Genotronex.println(value);
+}
+
+void printD(float value) {
+    Genotronex.write('D');
+    Genotronex.println(value);
 }
